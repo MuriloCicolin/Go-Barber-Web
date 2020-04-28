4cd0296cc4c9241e843f99f4a3f11a20d93f56f3
MuriloCicolin <mu_cicolin@hotmail.com>
2020-04-28 18:48:46 -0300

    usabilidade do input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -19,10 +19,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="Go Barber" />
 
-        <Form
-          initialData={{ name: 'Murilo', email: 'mu.cicolin@gmail.com' }}
-          onSubmit={handleSubmit}
-        >
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
