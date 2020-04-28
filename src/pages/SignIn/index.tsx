73c502a507632f908aead0615ab90d49b0c9f245
MuriloCicolin <mu_cicolin@hotmail.com>
2020-04-28 14:11:18 -0300

    Isolando componentes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
-import {FiLogIn} from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 const SignIn: React.FC = () => (
-   <Container>
-     <Content>
-      <img src={logoImg} alt="Go Barber"/>
+  <Container>
+    <Content>
+      <img src={logoImg} alt="Go Barber" />
 
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail"/>
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha"/>
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit" >Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
-
       </form>
 
       <a href="#">
         <FiLogIn />
         Criar conta
       </a>
-     </Content>
-     <Background />
-   </Container>
-)
+    </Content>
+    <Background />
+  </Container>
+);
 
 export default SignIn;
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -8,7 +8,6 @@ export const Container = styled.div`
 
   display: flex;
   align-items: stretch;
-
 `;
 
 export const Content = styled.div`
@@ -30,71 +29,36 @@ export const Content = styled.div`
     margin-bottom: 24px;
   }
 
-  input {
-    background: #232129;
-    border-radius: 10px;
-    border: 2px solid #232129;
-    padding: 16px;
-    width: 100%;
-    color: #F4EDE8;
-
-    &::placeholder {
-      color: #666360;
-    }
-
-    & + input {
-      margin-top: 8px;
-    }
-  }
-
-  button {
-    background: #ff9000;
-    height: 56px;
-    border-radius: 10px;
-    border: 0;
-    padding: 0 16px;
-    color: #312e38;
-    width: 100%;
-    font-weight: 500;
-    margin-top: 16px;
-    transition: background-color 0.2s;
+  a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
 
     &:hover {
-      background: ${shade(0.2, '#ff9000')};
+      color: ${shade(0.2, '#F4EDE8')};
     }
   }
-
-  a {
-      color: #F4EDE8;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 0.2s;
-
-      &:hover {
-        color: ${shade(0.2, '#F4EDE8')};
-      }
-    }
 
   > a {
-      color: #ff9000;
-      display: block;
-      margin-top: 24px;
-      text-decoration: none;
-      transition: color 0.2s;
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
 
-      display: flex;
-      align-items: center;
+    display: flex;
+    align-items: center;
 
-      &:hover {
-        color: ${shade(0.2, '#ff9000')};
-      }
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
 
-      svg {
-        margin-right: 16px;
-      }
+    svg {
+      margin-right: 16px;
+    }
   }
-
 `;
 
 export const Background = styled.div`
