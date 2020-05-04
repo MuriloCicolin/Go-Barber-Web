3a9d5ce26f17b3a0dca07be9ac2db74554823e8f
MuriloCicolin <mu_cicolin@hotmail.com>
2020-05-04 20:22:36 -0300

    Botao de logout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { Container } from './styles';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+  useCallback(() => {
+    signOut();
+  }, [signOut]);
+
+  return (
+    <>
+      <Container>
+        <h1>Dashboard</h1>
+
+        <button type="button" onClick={signOut}>
+          Logout
+        </button>
+      </Container>
+    </>
+  );
+};
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 100%;
+  align-items: center;
+  margin-top: 20px;
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    margin-top: 25px;
+    border: 0;
+    padding: 10px;
+    border-radius: 4px;
+    font-size: 18px;
+  }
+`;
